cf09a4f8abd40f4892e334ddf8be1378d89c3f96
Johannes Seiler <Johannes.Seiler@training.lug-ag.de>
2025-12-03 09:09:40 +0100

    ##Grundsystem, Mitglieder verwalten, Anwesenheit erfassen, Login / Benutzerverwaltung, Beenden, Mitglieder Liste anzeigen
    ID: 1Name: Max Mustermann, Tarif: Basic, Aktiv: True##

--- FitMaster/Program.cs
+++ FitMaster/Program.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 namespace FitMaster
 {
     // ----------------------------------------
-    // Model: Mitglieder
+    // Model: Mitglied
     // ----------------------------------------
     public class Member
     {
